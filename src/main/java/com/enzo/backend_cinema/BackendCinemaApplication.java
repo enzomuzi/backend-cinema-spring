0714860e13dca74d24df482067886e3cae3d25a6
enzomuzi <enzshadows@gmail.com>
2025-02-05 17:45:27 -0300

    feat-01: Configuração do banco de dados

--- src/main/java/com/enzo/backend_cinema/BackendCinemaApplication.java
+++ src/main/java/com/enzo/backend_cinema/BackendCinemaApplication.java
@@ -1,7 +1,12 @@
 package com.enzo.backend_cinema;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.enzo.backend_cinema.model.Film;
+import com.enzo.backend_cinema.repository.FilmRepository;
 
 @SpringBootApplication
 public class BackendCinemaApplication {
@@ -9,5 +14,19 @@ public class BackendCinemaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendCinemaApplication.class, args);
 	}
+@Bean
+CommandLineRunner initDatabase(FilmRepository filmRepository) {
+	return args -> {
+		filmRepository.deleteAll();
+
+		Film f = new Film();
+		f.setName("Aviões");
+		f.setDescription("Filme de criança");
+		f.setHours("20H00");
+
+		filmRepository.save(f);
+	};
+}
+	
 
 }
---
+++ src/main/java/com/enzo/backend_cinema/repository/FilmRepository.java
@@ -0,0 +1,12 @@
+package com.enzo.backend_cinema.repository;
+
+import com.enzo.backend_cinema.model.Film;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Long> {
+    
+}
